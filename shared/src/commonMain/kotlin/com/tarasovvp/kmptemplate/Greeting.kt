937694c06054704df344fdaf1c227b31ecee9b32
TarasovVP <vptarasov1998@gmail.com>
2025-07-10 21:24:59 +0300

    Add formatted date time implementation

--- shared/src/commonMain/kotlin/com/tarasovvp/kmptemplate/Greeting.kt
+++ shared/src/commonMain/kotlin/com/tarasovvp/kmptemplate/Greeting.kt
@@ -1,9 +1,28 @@
 package com.tarasovvp.kmptemplate
 
+import kotlinx.datetime.*
+import kotlinx.datetime.format.*
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val currentDateTime = Clock.System.now()
+            .toLocalDateTime(TimeZone.currentSystemDefault())
+
+        val formatter = LocalDateTime.Format {
+            dayOfMonth()
+            chars("-")
+            monthNumber()
+            chars("-")
+            year()
+            chars(" ")
+            hour()
+            chars(":")
+            minute()
+        }
+
+        val formattedDateTime = currentDateTime.format(formatter)
+        return "Hello, ${platform.name}! Now is $formattedDate"
     }
 }
