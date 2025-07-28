4518698227f5d8ac1cef9027fafa66fbba6a7e45
TarasovVP <vptarasov1998@gmail.com>
2025-07-28 17:21:17 +0300

    Add demo object model

---
+++ shared/src/commonMain/kotlin/com/tarasovvp/kmptemplate/domain/model/DemoObject.kt
@@ -0,0 +1,7 @@
+package com.tarasovvp.kmptemplate.domain.model
+
+data class DemoObject(
+    var demoObjectId: String? = null,
+    var name: String? = null,
+    var description: String? = null,
+)
